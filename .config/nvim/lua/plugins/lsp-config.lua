3c25dd987d150cb217d6f63199221d2cbdbd2d72
WhiteToggled <toggledwhite@gmail.com>
2025-09-18 20:40:05 +0500

    refactor: refactor require(lspconfig) to vim.lsp.config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,48 +1,69 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "rust_analyzer" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilites,
-			})
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-			})
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "clangd", "rust_analyzer" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- local pid = vim.fn.getpid();
-            local omnisharp_path = "/home/whitetoggled/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll";
-			lspconfig.omnisharp.setup({
-                cmd = {"dotnet", omnisharp_path},
-                -- *.cs is only a workaround
-                root_dir = require("lspconfig.util").root_pattern('*.sln', '*.csproj', 'omnisharp.json', 'function.json'),
-				capabilities = capabilities,
-			})
-		end,
-	},
+      -- Lua
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("lua_ls")
+
+      -- Clang
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("clangd")
+
+      -- TS/JS (ts-ls)
+      vim.lsp.config("tsserver", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("tsserver")
+
+      -- Rust
+      vim.lsp.config("rust_analyzer", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("rust_analyzer")
+
+      -- Python
+      vim.lsp.config("pylsp", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("pylsp")
+
+      -- Assembly
+      -- vim.lsp.config("asm_lsp", {
+      --   capabilities = capabilities,
+      -- })
+      -- vim.lsp.enable("asm_lsp")
+
+      -- Omnisharp for C#
+      -- local pid = vim.fn.getpid();
+      local omnisharp_path = "/home/whitetoggled/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll";
+      vim.lsp.config("omnisharp", {
+        cmd = {"dotnet", omnisharp_path},
+        -- *.cs is only a workaround
+        root_dir = vim.fs.root(0, { "*.sln", "*.csproj", "omnisharp.json", "function.json" }),
+        capabilities = capabilities,
+      })
+    end,
+  },
 }
